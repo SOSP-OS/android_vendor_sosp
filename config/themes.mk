7279aebfd7b0fb4dd6e97b280bf6ab88b696716a
Gnathvm <gnathvm@gmail.com>
2022-08-21 09:26:01 +0530

    config: Build font overlays again

--- config/themes.mk
+++ config/themes.mk
@@ -18,7 +18,15 @@ PRODUCT_PACKAGES += \
     ThemeStub
     
     
-    
+# Include {Lato,Rubik} fonts
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+
+# Fonts
+PRODUCT_PACKAGES += \
+    fonts_customization.xml \
+    FontLatoOverlay \
+    FontRubikOverlay   
     
     
     
