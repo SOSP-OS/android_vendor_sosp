2ffdeeace95070e47837eef30f6a955d590de6cd
Gnathvm <gnathvm@gmail.com>
2022-09-10 19:11:05 +0530

    vendor: Spoof build fingerprint for Google Play Services [2/2]

--- config/props.mk
+++ config/props.mk
@@ -37,4 +37,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     net.tethering.noprovisioning=true \
     ro.config.calibration_cad=/system/etc/calibration_cad.xml
     
-
+# Spoof fingerprint for Google Play Services and SafetyNet
+ifeq ($(PRODUCT_OVERRIDE_GMS_FINGERPRINT),)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys
+else
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=$(PRODUCT_OVERRIDE_GMS_FINGERPRINT)
+endif
