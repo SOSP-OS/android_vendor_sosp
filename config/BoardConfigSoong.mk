5577ad0ba401d884aa043c40343c0e2ae7b8eb6b
Gnathvm <gnathvm@gmail.com>
2023-10-24 11:07:35 +0530

    soong: Add TARGET_USES_EGL_DISPLAY_ARRAY conditional
    This soong variable is used to conditionally revert a commit [1]
    which causes random camera crashes on tama devices

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -32,7 +32,8 @@ SOONG_CONFIG_sospGlobalVars += \
     gralloc_handle_has_reserved_size \
     target_init_vendor_lib \
     target_ld_shim_libs \
-    target_surfaceflinger_udfps_lib
+    target_surfaceflinger_udfps_lib \
+    uses_egl_display_array
 
 SOONG_CONFIG_NAMESPACES += sospNvidiaVars
 SOONG_CONFIG_sospNvidiaVars += \
@@ -52,6 +53,7 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_sospGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
+SOONG_CONFIG_sospGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_sospNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_sospQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_sospQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
